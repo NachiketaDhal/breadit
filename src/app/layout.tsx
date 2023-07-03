9c0619816f238694034714dcd0867cf9f0370069
Nachiketa Dhal <bnachiketa26@gmail.com>
2023-07-04 00:06:37 +0530

    Implement route interception

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ const inter = Inter({
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -28,6 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen pt-12 antialiased bg-slate-50">
         <Navbar />
+        {authModal}
         <div className="container h-full pt-12 mx-auto max-w-7xl">
           {children}
         </div>
